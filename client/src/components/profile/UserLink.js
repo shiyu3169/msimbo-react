d9b728eebf0fa2591888e639661585577134401d
Shiyu Wang <shiyu3169@gmail.com>
2018-11-05 15:02:37 -0500

    show user link

--- client/src/components/profile/UserLink.js
+++ client/src/components/profile/UserLink.js
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default function UserLink() {
-    return (
-        <React.Fragment>
-            <h6>
-                <i className="fas fa-project-diagram" /> Project:{" "}
-                <a className="sw-profile" href="#" target="_blank">
-                    project
-                </a>
-            </h6>
-            <h6>
-                <i className="fab fa-linkedin fa-lg" /> LinkedIn:{" "}
-                <a className="sw-profile" href="#" target="_blank">
-                    linkedin
-                </a>
-            </h6>
-            <h6>
-                <i className="fab fa-github fa-lg" /> GitHub:{" "}
-                <a className="sw-profile" href="github" target="_blank">
-                    github
-                </a>
-            </h6>
-            <br />
-            <button
-                className="btn btn-outline-info"
-                data-toggle="modal"
-                data-target="#linkModal"
-            >
-                Edit Links
-            </button>
-        </React.Fragment>
-    );
+class UserLink extends Component {
+    render() {
+        const { user } = this.props;
+        return (
+            <React.Fragment>
+                <h6>
+                    <i className="fas fa-project-diagram" /> Project:{" "}
+                    <a className="sw-profile" href="#" target="_blank">
+                        {user.project}
+                    </a>
+                </h6>
+                <h6>
+                    <i className="fab fa-linkedin fa-lg" /> LinkedIn:{" "}
+                    <a className="sw-profile" href="#" target="_blank">
+                        {user.linkedin}
+                    </a>
+                </h6>
+                <h6>
+                    <i className="fab fa-github fa-lg" /> GitHub:{" "}
+                    <a className="sw-profile" href="github" target="_blank">
+                        {user.github}
+                    </a>
+                </h6>
+                <br />
+                <button
+                    className="btn btn-outline-info"
+                    data-toggle="modal"
+                    data-target="#linkModal"
+                >
+                    Edit Links
+                </button>
+            </React.Fragment>
+        );
+    }
 }
+
+const mapStateToProps = state => ({
+    user: state.user.user
+});
+
+export default connect(
+    mapStateToProps,
+    {}
+)(UserLink);
