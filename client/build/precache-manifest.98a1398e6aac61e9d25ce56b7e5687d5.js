1122ff1d9a8f6b81ea35e09161128cf4bcae8408
Shiyu Wang <shiyu3169@gmail.com>
2018-11-05 15:37:52 -0500

    build locally

---
+++ client/build/precache-manifest.98a1398e6aac61e9d25ce56b7e5687d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c58897a74b7316c9443acc494376ff5",
+    "url": "/static/media/showcase.2c58897a.jpg"
+  },
+  {
+    "revision": "e275bf6b681fd7108a7ddcd22b640097",
+    "url": "/static/media/loading.e275bf6b.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9c8c790e6ad3a22ab1d",
+    "url": "/static/js/main.b9c8c790.chunk.js"
+  },
+  {
+    "revision": "be933fefb87e96f8b06f",
+    "url": "/static/js/1.be933fef.chunk.js"
+  },
+  {
+    "revision": "b9c8c790e6ad3a22ab1d",
+    "url": "/static/css/main.b4ad7112.chunk.css"
+  },
+  {
+    "revision": "be933fefb87e96f8b06f",
+    "url": "/static/css/1.cc35ab8d.chunk.css"
+  },
+  {
+    "revision": "5b7fe0032d0e304910ac74cf9c726250",
+    "url": "/index.html"
+  }
+];
