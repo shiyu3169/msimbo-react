02c86110c34cb0f69955b6eb7d0bc82c5b926774
Shiyu Wang <shiyu3169@gmail.com>
2018-11-07 11:25:58 -0500

    remove animation

--- client/src/components/Curriculum.js
+++ client/src/components/Curriculum.js
@@ -2,13 +2,11 @@ import React from "react";
 
 export default function Curriculum() {
     return (
-        <div className="holds-the-iframe">
-            <iframe
-                title="curriculum"
-                frameBorder="0"
-                className="sw-sheet"
-                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzOcfElFHVJnt9xb_VFg2jpHxRwVywEPHH1q4XdkbksWgbkWnq139eKYWxGrVnKAcxK1ybf-e1yIiR/pubhtml?chrome=false&widget=true&amp;headers=false"
-            />
-        </div>
+        <iframe
+            title="curriculum"
+            frameBorder="0"
+            className="sw-sheet"
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzOcfElFHVJnt9xb_VFg2jpHxRwVywEPHH1q4XdkbksWgbkWnq139eKYWxGrVnKAcxK1ybf-e1yIiR/pubhtml?chrome=false&widget=true&amp;headers=false"
+        />
     );
 }
