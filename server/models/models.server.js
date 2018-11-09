cbb3d8abc907c76f4ca638af3aa4f68d3468ca16
Shiyu Wang <shiyu3169@gmail.com>
2018-11-09 13:31:43 -0500

    change db

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -5,7 +5,7 @@ if (process.env.MLAB_USERNAME_WEBDEV) {
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = "mongodb://" + username + ":" + password;
-    connectionString += "@ds117899.mlab.com:17899/heroku_xmndv6qm"; // use yours
+    connectionString += "@ds259210.mlab.com:59210/heroku_90kxmhrc"; // use yours
 }
 
 var mongoose = require("mongoose");
