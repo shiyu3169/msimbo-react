c520f4b1c7940227e3891f1ceb6ec25bfaf9123b
Shiyu Wang <shiyu3169@gmail.com>
2018-11-16 15:51:24 -0500

    action and reducer for resources

--- client/src/actions/resourceActions.js
+++ client/src/actions/resourceActions.js
@@ -1,4 +1,11 @@
-import { GET_RESOURCES } from "./types";
+import {
+    GET_RESOURCES,
+    CREATE_RESOURCE,
+    ADD_RESOURCE,
+    EDIT_RESOURCE,
+    DELETE_RESOURCE,
+    UPDATE_RESOURCE
+} from "./types";
 import axios from "axios";
 
 export const getResources = () => async dispatch => {
@@ -6,5 +13,42 @@ export const getResources = () => async dispatch => {
     dispatch({
         type: GET_RESOURCES,
         payload: res.data
+    });
+};
+
+export const createReousrce = () => async dispatch => {
+    dispatch({
+        type: CREATE_RESOURCE
+    });
+};
+
+export const addResource = resource => async dispatch => {
+    const res = await axios.post("/api/resource", resource);
+    dispatch({
+        type: ADD_RESOURCE,
+        payload: res.data
+    });
+};
+
+export const editResource = id => async dispatch => {
+    dispatch({
+        type: EDIT_RESOURCE,
+        payload: id
+    });
+};
+
+export const deleteResource = id => async dispatch => {
+    await axios.delete(`/api/resource/${id}`);
+    dispatch({
+        type: DELETE_RESOURCE,
+        payload: id
+    });
+};
+
+export const updateResource = resource => async dispatch => {
+    await axios.put(`/api/resource/${resource._id}`, resource);
+    dispatch({
+        type: UPDATE_RESOURCE,
+        payload: resource
     });
 };
--- client/src/reducers/resourceReducer.js
+++ client/src/reducers/resourceReducer.js
@@ -1,7 +1,16 @@
-import { GET_RESOURCES } from "../actions/types";
+import {
+    GET_RESOURCES,
+    CREATE_RESOURCE,
+    ADD_RESOURCE,
+    EDIT_RESOURCE,
+    DELETE_RESOURCE,
+    UPDATE_RESOURCE
+} from "../actions/types";
 
 const initialState = {
-    resources: []
+    resources: [],
+    creating: false,
+    editing: ""
 };
 
 export default function(state = initialState, action) {
@@ -10,6 +19,37 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 resources: action.payload
+            };
+        case CREATE_RESOURCE:
+            return {
+                ...state,
+                creating: !state.creating
+            };
+        case ADD_RESOURCE:
+            return {
+                ...state,
+                resources: [action.payload, ...state.resources]
+            };
+        case EDIT_RESOURCE:
+            return {
+                ...state,
+                editing: action.payload
+            };
+        case DELETE_RESOURCE:
+            return {
+                ...state,
+                resources: state.resources.filter(
+                    resource => resource._id !== action.payload
+                )
+            };
+        case UPDATE_RESOURCE:
+            return {
+                ...state,
+                resources: state.resources.map(resource =>
+                    resource._id === action.payload._id
+                        ? (resource = action.payload)
+                        : resource
+                )
             };
         default:
             return state;
