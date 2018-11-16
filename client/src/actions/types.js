b437c9a5d70d2a7f68d7ac4aea42f175ff332d1e
Shiyu Wang <shiyu3169@gmail.com>
2018-11-16 15:42:44 -0500

    adding types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -9,7 +9,12 @@ export const EDIT_USER = "EDIT";
 export const UPDATE_USER = "UPDATE";
 // Resource
 export const GET_RESOURCES = "GET_RESOURCES";
-//Grade
+export const CREATE_RESOURCE = "CREATE_RESOURCE";
+export const ADD_RESOURCE = "ADD_RESOURCE";
+export const EDIT_RESOURCE = "EDIT_RESOURCE";
+export const UPDATE_RESOURCE = "UPDATE_RESOURCE";
+export const DELETE_RESOURCE = "DELETE_RESOURCE";
+// Grade
 export const GET_GRADES_BY_USER = "GET_GRADES_BY_USER";
 export const ADD_GRADE = "ADD_GRADE";
 export const DELETE_GRADE = "DELETE_GRADE";
