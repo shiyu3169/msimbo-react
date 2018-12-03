ee028b403496c8e7f841a67d4948ae2ae53184da
Shiyu Wang <shiyu3169@gmail.com>
2018-12-03 14:32:30 -0500

    display videos

--- client/src/components/youtube/Youtube.js
+++ client/src/components/youtube/Youtube.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 export default class Youtube extends Component {
     render() {
-        const { youtube } = this.props;
+        const { video } = this.props;
         return (
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe
-                    class="embed-responsive-item"
-                    src={youtube.src}
-                    allowfullscreen
-                />
-            </div>
+            <React.Fragment>
+                <h1>{video.name}</h1>
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe
+                        className="embed-responsive-item"
+                        src={video.src}
+                        allowFullScreen
+                        title={video.name}
+                    />
+                </div>
+            </React.Fragment>
         );
     }
 }
--- client/src/components/youtube/Youtubes.js
+++ client/src/components/youtube/Youtubes.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { getVideos } from "../../actions/videoActions";
+import Youtube from "./Youtube";
 
 class Youtubes extends Component {
+    componentDidMount() {
+        this.props.getVideos();
+    }
+
     render() {
-        return <div>123</div>;
+        const { videos } = this.props;
+        return (
+            <div className="sw-bg-white">
+                <div className="container">
+                    {videos.map(video => (
+                        <Youtube key={video._id} video={video} />
+                    ))}
+                </div>
+            </div>
+        );
     }
 }
 
+const mapStateToProps = state => ({
+    videos: state.video.videos,
+    creating: state.resource.creating
+});
+
 export default connect(
-    null,
-    {}
+    mapStateToProps,
+    { getVideos }
 )(Youtubes);
