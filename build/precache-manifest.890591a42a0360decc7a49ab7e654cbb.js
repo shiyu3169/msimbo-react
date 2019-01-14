d89c43df5d4e68a165c8b0aa65f015d4524c5a1e
Shiyu Wang <shiyu3169@gmail.com>
2019-01-14 15:20:16 -0500

    build

---
+++ build/precache-manifest.890591a42a0360decc7a49ab7e654cbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c58897a74b7316c9443acc494376ff5",
+    "url": "/static/media/showcase.2c58897a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99adbc8d69b31b182500",
+    "url": "/static/js/main.99adbc8d.chunk.js"
+  },
+  {
+    "revision": "b71c68454007b8de52ef",
+    "url": "/static/js/1.b71c6845.chunk.js"
+  },
+  {
+    "revision": "99adbc8d69b31b182500",
+    "url": "/static/css/main.ffe83d43.chunk.css"
+  },
+  {
+    "revision": "b71c68454007b8de52ef",
+    "url": "/static/css/1.a13f03ba.chunk.css"
+  },
+  {
+    "revision": "e3c0266b96833565f4fe205c1fb5d38e",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ca8a09738ad88a2903bb713a419ee15.js"
+  "/precache-manifest.890591a42a0360decc7a49ab7e654cbb.js"
 );
 
 workbox.clientsClaim();
