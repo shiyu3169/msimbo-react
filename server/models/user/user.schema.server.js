35d03ef412ab83ad3a86c161fc2cbe68883c7eb2
Shiyu Wang <shiyu3169@gmail.com>
2019-01-14 14:54:33 -0500

    register with email

--- server/models/user/user.schema.server.js
+++ server/models/user/user.schema.server.js
@@ -1,21 +1,21 @@
 var mongoose = require("mongoose");
 
 var UserSchema = mongoose.Schema(
-    {
-        username: String,
-        password: String,
-        firstName: { type: String, default: "" },
-        lastName: { type: String, default: "" },
-        email: { type: String, default: "" },
-        image: { type: String, default: "logo.png" },
-        admin: { type: Boolean, default: false },
-        bio: { type: String, default: "MSIMBO Student" },
-        github: { type: String, default: "" },
-        linkedin: { type: String, default: "" },
-        project: { type: String, default: "" },
-        dateCreated: { type: Date, default: Date.now }
-    },
-    { collection: "user" }
+  {
+    password: { type: String, required: true },
+    firstName: { type: String, default: "" },
+    middleName: { type: String, default: "" },
+    lastName: { type: String, default: "" },
+    email: { type: String, required: true },
+    image: { type: String, default: "logo.png" },
+    admin: { type: Boolean, default: false },
+    bio: { type: String, default: "MSIMBO Student" },
+    github: { type: String, default: "" },
+    linkedin: { type: String, default: "" },
+    project: { type: String, default: "" },
+    dateCreated: { type: Date, default: Date.now }
+  },
+  { collection: "user" }
 );
 
 module.exports = UserSchema;
