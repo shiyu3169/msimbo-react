d41eaae59afa18df311a365cf4506082a6ec5452
Shiyu Wang <shiyu3169@gmail.com>
2019-01-30 11:35:13 -0500

    update curriculum

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -1,12 +1,605 @@
 import React from "react";
 
 export default function Curriculum() {
-    return (
-        <iframe
-            title="curriculum"
-            frameBorder="0"
-            className="sw-sheet"
-            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRzOcfElFHVJnt9xb_VFg2jpHxRwVywEPHH1q4XdkbksWgbkWnq139eKYWxGrVnKAcxK1ybf-e1yIiR/pubhtml?chrome=false&widget=true&amp;headers=false"
-        />
-    );
+  return (
+    <div className="table-responsive">
+      <table className="sw-bg-white table text-center table-bordered">
+        <thead>
+          <tr>
+            <th>Module</th>
+            <th>Week</th>
+            <th>Date</th>
+            <th>Weekday</th>
+            <th>Topic</th>
+            <th>Assignment</th>
+            <th>Quiz</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="align-middle" rowSpan="10">
+              HTML/CSS
+            </td>
+            <td rowSpan="5" className="align-middle">
+              1
+            </td>
+            <td>3/4</td>
+            <td>Monday</td>
+            <td>
+              Full-Stack Development, LinkedIn, MSIMBO Website, Slack, Syllabus,
+              Job Oppotunities,{" "}
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud206&sa=D&ust=1548782836843000&usg=AFQjCNHdxVIejY4AbbAVxi6mlIKTZR6AVA">
+                Shell Workshop
+              </a>
+            </td>
+            <td>
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud775&sa=D&ust=1548782836844000&usg=AFQjCNHSMyCxG5OT9aAj2U-JMz5Gn5hAqg">
+                How to use Git and GitHub
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>3/5</td>
+            <td>Tuesday</td>
+            <td>Git, GitHub</td>
+            <td>
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud001&sa=D&ust=1548782836844000&usg=AFQjCNHkZDUP0CzWaMBFc_WQp6QjSY5TWQ">
+                HTML
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>3/6</td>
+            <td>Wednesday</td>
+            <td>HTML</td>
+            <td>
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud001&sa=D&ust=1548782836844000&usg=AFQjCNHkZDUP0CzWaMBFc_WQp6QjSY5TWQ">
+                CSS
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>3/7</td>
+            <td>Thursday</td>
+            <td>CSS</td>
+            <td>Assignment 1</td>
+          </tr>
+          <tr>
+            <td>3/8</td>
+            <td>Friday</td>
+            <td>Bootstrap</td>
+            <td>Assignment 2</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              2
+            </td>
+            <td>3/11</td>
+            <td>Monday</td>
+            <td rowSpan="5" className="align-middle">
+              HTML, CSS, Bootstrap
+            </td>
+          </tr>
+          <tr>
+            <td>3/12</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>3/13</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>3/14</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>3/15</td>
+            <td>Friday</td>
+            <td>
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud803&sa=D&ust=1548782836846000&usg=AFQjCNGGY5CDEdm7JVqPUNlecBdepOHOUg">
+                Intro to JavaScript
+              </a>
+            </td>
+          </tr>
+          <tr />
+          <tr>
+            <td className="align-middle" rowSpan="26">
+              JavaScript
+            </td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              3
+            </td>
+            <td>3/18</td>
+            <td>Monday</td>
+            <td>Overview, Basic Concepts, Data Types, Variables</td>
+            <td />
+            <td>Quiz 1</td>
+          </tr>
+          <tr>
+            <td>3/19</td>
+            <td>Tuesday</td>
+            <td>Conditionals</td>
+          </tr>
+          <tr>
+            <td>3/20</td>
+            <td>Wednesday</td>
+            <td>Loops</td>
+          </tr>
+          <tr>
+            <td>3/21</td>
+            <td>Thursday</td>
+            <td>Functions</td>
+          </tr>
+          <tr>
+            <td>3/22</td>
+            <td>Friday</td>
+            <td>Arrays</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              4
+            </td>
+            <td>3/25</td>
+            <td>Monday</td>
+            <td>Objects</td>
+            <td />
+            <td>Assignment 3</td>
+          </tr>
+          <tr>
+            <td>3/26</td>
+            <td>Tuesday</td>
+            <td>DOM</td>
+          </tr>
+          <tr>
+            <td>3/27</td>
+            <td>Wednesday</td>
+            <td>Events</td>
+          </tr>
+          <tr>
+            <td>3/28</td>
+            <td>Thursday</td>
+            <td>Filterable List</td>
+          </tr>
+          <tr>
+            <td>3/29</td>
+            <td>Friday</td>
+            <td>Unit Converter</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              5
+            </td>
+            <td>3/4/1</td>
+            <td>Monday</td>
+            <td>Bookmarker</td>
+          </tr>
+          <tr>
+            <td>4/2</td>
+            <td>Tuesday</td>
+            <td>Todo List</td>
+            <td>
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud356&sa=D&ust=1548787999783000&usg=AFQjCNE9tC1AC3hhgbVA4xBYpqd8OQaxBw">
+                ES6
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>4/3</td>
+            <td>Wednesday</td>
+            <td>ES6</td>
+          </tr>
+          <tr>
+            <td>4/4</td>
+            <td>Thursday</td>
+            <td>Update Projects</td>
+            <td>
+              <a href="https://www.google.com/url?q=https://classroom.udacity.com/courses/ud245&sa=D&ust=1548787999783000&usg=AFQjCNGm2jbbzfDSjYxlUxWD3Vlyo-96Vw">
+                Intro to jQuery
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>4/5</td>
+            <td>Friday</td>
+            <td>jQuery</td>
+            <td>
+              <a href="https://www.google.com/url?q=https://www.udacity.com/course/javascript-design-patterns--ud989&sa=D&ust=1548787999784000&usg=AFQjCNFOr3aEEzUxgYQrY_9mu15IV_6NRw">
+                JavaScript Design Patterns
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              6
+            </td>
+            <td>4/8</td>
+            <td>Monday</td>
+            <td rowSpan="10" className="align-middle">
+              JavaScript Design Patterns
+            </td>
+          </tr>
+          <tr>
+            <td>4/9</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>4/10</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>4/11</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>4/12</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              7
+            </td>
+            <td>4/15</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>4/16</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>4/17</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>4/18</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>4/19</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td className="align-middle" rowSpan="10">
+              Client Side Development
+            </td>
+            <td rowSpan="5" className="align-middle">
+              8
+            </td>
+            <td>4/22</td>
+            <td>Monday</td>
+            <td rowSpan="10" className="align-middle">
+              React, Component, Routing
+            </td>
+            <td>Assignment 4</td>
+            <td>Quiz 2</td>
+          </tr>
+          <tr>
+            <td>4/23</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>4/24</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>4/25</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>4/26</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              9
+            </td>
+            <td>4/29</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>4/30</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>5/1</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>5/2</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>5/3</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td className="align-middle" rowSpan="10">
+              Server Side Development
+            </td>
+            <td rowSpan="5" className="align-middle">
+              10
+            </td>
+            <td>5/6</td>
+            <td>Monday</td>
+            <td rowSpan="10" className="align-middle">
+              Fetch, Node.js, Express.js, REST, Asynchronous, Web Services
+            </td>
+            <td>Assignment 5</td>
+            <td>Quiz 3</td>
+          </tr>
+          <tr>
+            <td>5/7</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>5/8</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>5/9</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>5/10</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              11
+            </td>
+            <td>5/13</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>5/14</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>5/15</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>5/16</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>5/17</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td className="align-middle" rowSpan="10">
+              Database Development
+            </td>
+            <td rowSpan="5" className="align-middle">
+              12
+            </td>
+            <td>5/20</td>
+            <td>Monday</td>
+            <td rowSpan="10" className="align-middle">
+              NoSQL, MongoDB, Querying/Updating Data, Mongoose, Schemas, Models,
+              Querying/Updating MongoDB
+            </td>
+            <td>Assignment 6</td>
+            <td>Quiz 4</td>
+          </tr>
+          <tr>
+            <td>5/21</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>5/22</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>5/23</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>5/24</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              13
+            </td>
+            <td>5/27</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>5/28</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>5/29</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>5/30</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>5/31</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td className="align-middle" rowSpan="10">
+              Security Development
+            </td>
+            <td rowSpan="5" className="align-middle">
+              14
+            </td>
+            <td>6/3</td>
+            <td>Monday</td>
+            <td className="align-middle" rowSpan="10">
+              PassportJS, Securing Web APIs, Encrypt password, User Management
+            </td>
+            <td>Assignment 7</td>
+            <td>Quiz 5</td>
+          </tr>
+          <tr>
+            <td>6/4</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>6/5</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>6/6</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>6/7</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              15
+            </td>
+            <td>6/10</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>6/11</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>6/12</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>6/13</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>6/14</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td className="align-middle" rowSpan="25">
+              Project & Feature Libraries
+            </td>
+            <td rowSpan="5" className="align-middle">
+              16
+            </td>
+            <td>6/17</td>
+            <td>Monday</td>
+            <td>Chart, Oath2, Animation, Image Upload</td>
+            <td>Project</td>
+            <td>Quiz 6</td>
+          </tr>
+          <tr>
+            <td>6/18</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>6/19</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>6/20</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>6/21</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              17
+            </td>
+            <td>6/24</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>6/25</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>6/26</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>6/27</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>6/28</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              18
+            </td>
+            <td>7/1</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>7/2</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>7/3</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>7/4</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>7/5</td>
+            <td>Friday</td>
+          </tr>
+          <tr />
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              19
+            </td>
+            <td>7/8</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>7/9</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>7/10</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>7/11</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>7/12</td>
+            <td>Friday</td>
+          </tr>
+          <tr>
+            <td rowSpan="5" className="align-middle">
+              20
+            </td>
+            <td>7/15</td>
+            <td>Monday</td>
+          </tr>
+          <tr>
+            <td>7/16</td>
+            <td>Tuesday</td>
+          </tr>
+          <tr>
+            <td>7/17</td>
+            <td>Wednesday</td>
+          </tr>
+          <tr>
+            <td>7/18</td>
+            <td>Thursday</td>
+          </tr>
+          <tr>
+            <td>7/19</td>
+            <td>Friday</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
