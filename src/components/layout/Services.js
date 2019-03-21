14427f0b9097d86f523c17e4757f5a93428b0a34
Shiyu Wang <shiyu3169@gmail.com>
2019-03-21 15:29:40 -0400

    fix assignments link

--- src/components/layout/Services.js
+++ src/components/layout/Services.js
@@ -3,79 +3,77 @@ import ServiceIcon from "./ServiceIcon";
 import { Redirect } from "react-router-dom";
 
 class Services extends Component {
-    state = {
-        redirect: false,
-        path: ""
-    };
+  state = {
+    redirect: false,
+    path: ""
+  };
 
-    navigate = url => {
-        var win = window.open(url, "_blank");
-        win.focus();
-    };
+  navigate = url => {
+    var win = window.open(url, "_blank");
+    win.focus();
+  };
 
-    redirect = path => {
-        this.setState({
-            redirect: true,
-            path: path
-        });
-    };
+  redirect = path => {
+    this.setState({
+      redirect: true,
+      path: path
+    });
+  };
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect push to={this.state.path} />;
-        }
-
-        return (
-            <React.Fragment>
-                <section className="services">
-                    <div className="container grid-3 center">
-                        <ServiceIcon
-                            iconClass="fas fa-users fa-3x"
-                            title="Students"
-                            description="Students list"
-                            onClick={this.redirect.bind(this, "/students")}
-                        />
-                        <ServiceIcon
-                            iconClass="fas fa-graduation-cap fa-3x"
-                            title="Curriculum"
-                            description="Course Schedule"
-                            onClick={this.redirect.bind(this, "/curriculum")}
-                        />
-                        <ServiceIcon
-                            iconClass="fab fa-youtube fa-3x"
-                            title="Videos"
-                            description="Class Videos on Youtube"
-                            onClick={this.redirect.bind(this, "/videos")}
-                        />
-                    </div>
-                </section>
-                <section className="services">
-                    <div className="container grid-3 center">
-                        <ServiceIcon
-                            iconClass="fas fa-pencil-ruler fa-3x"
-                            title="Assignments"
-                            description="Student Assignments"
-                        />
-                        <ServiceIcon
-                            iconClass="fas fa-archive fa-3x"
-                            title="Resources"
-                            description="Course slides and materials"
-                            onClick={this.redirect.bind(this, "/resources")}
-                        />
-                        <ServiceIcon
-                            iconClass="fab fa-slack fa-3x"
-                            title="Slack"
-                            description="Contact instructor or students"
-                            onClick={this.navigate.bind(
-                                this,
-                                "https://msimbo.slack.com"
-                            )}
-                        />
-                    </div>
-                </section>
-            </React.Fragment>
-        );
+  render() {
+    if (this.state.redirect) {
+      return <Redirect push to={this.state.path} />;
     }
+
+    return (
+      <React.Fragment>
+        <section className="services">
+          <div className="container grid-3 center">
+            <ServiceIcon
+              iconClass="fas fa-users fa-3x"
+              title="Students"
+              description="Students list"
+              onClick={this.redirect.bind(this, "/students")}
+            />
+            <ServiceIcon
+              iconClass="fas fa-graduation-cap fa-3x"
+              title="Curriculum"
+              description="Course Schedule"
+              onClick={this.redirect.bind(this, "/curriculum")}
+            />
+            <ServiceIcon
+              iconClass="fab fa-youtube fa-3x"
+              title="Videos"
+              description="Class Videos on Youtube"
+              onClick={this.redirect.bind(this, "/videos")}
+            />
+          </div>
+        </section>
+        <section className="services">
+          <div className="container grid-3 center">
+            <ServiceIcon
+              iconClass="fas fa-pencil-ruler fa-3x"
+              title="Assignments"
+              description="Student Assignments"
+              onClick={this.redirect.bind(this, "/assignments")}
+            />
+            <ServiceIcon
+              iconClass="fas fa-archive fa-3x"
+              title="Resources"
+              description="Course slides and materials"
+              onClick={this.redirect.bind(this, "/resources")}
+            />
+            <ServiceIcon
+              iconClass="fab fa-slack fa-3x"
+              title="Slack"
+              description="Contact instructor or students"
+              onClick={this.navigate.bind(this, "https://msimbo.slack.com")}
+            />
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Services;
