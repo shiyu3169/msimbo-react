32f1482cc944d08ad8449d1cc2902a6bbebd5521
Shiyu Wang <shiyu3169@gmail.com>
2019-03-22 10:07:16 -0400

    fix edit button

--- src/components/grades/Grade.js
+++ src/components/grades/Grade.js
@@ -1,35 +1,47 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Comment from "./Comment";
 import EditGrade from "./EditGrade";
-export default class Grade extends Component {
-    render() {
-        const { name, score, _id, comment } = this.props.grade;
-        const { grade } = this.props;
-        return (
-            <tr>
-                <td>{name}</td>
-                <td>{score}</td>
-                <td>
-                    {comment && (
-                        <button
-                            className="btn btn-outline-info"
-                            data-toggle="modal"
-                            data-target={`#c${_id}`}
-                        >
-                            Show Comment
-                        </button>
-                    )}
-                    <button
-                        className="btn btn-outline-warning float-right"
-                        data-toggle="modal"
-                        data-target={`#edit${_id}`}
-                    >
-                        <i className="fas fa-edit" />
-                    </button>
-                    <Comment grade={grade} />
-                    <EditGrade grade={grade} />
-                </td>
-            </tr>
-        );
-    }
+class Grade extends Component {
+  render() {
+    const { name, score, _id, comment } = this.props.grade;
+    const { grade } = this.props;
+    return (
+      <tr>
+        <td>
+          {name}
+        </td>
+        <td>
+          {score}
+        </td>
+        <td>
+          {comment &&
+            <button
+              className="btn btn-outline-info"
+              data-toggle="modal"
+              data-target={`#c${_id}`}
+            >
+              Show Comment
+            </button>}
+
+          {this.props.currentUser.admin &&
+            <button
+              className="btn btn-outline-warning float-right"
+              data-toggle="modal"
+              data-target={`#edit${_id}`}
+            >
+              <i className="fas fa-edit" />
+            </button>}
+          <Comment grade={grade} />
+          <EditGrade grade={grade} />
+        </td>
+      </tr>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps, {})(Grade);
