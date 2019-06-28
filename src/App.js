332924a95ce05220f6213cb833ca53bf55d5d160
Shiyu Wang <shiyu3169@gmail.com>
2019-06-28 14:43:40 -0400

    add quiz component and rewrite navbar into function component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // Layout
-import Navbar from "./components/layout/Navbar";
+import Navbar from './components/layout/Navbar';
 // Components
-import Home from "./components/Home";
-import Students from "./components/students/Students";
-import Resources from "./components/resources/Resources";
-import Profile from "./components/profile/Profile";
-import Assignments from "./components/assignments/Assignments";
-import Youtubes from "./components/youtube/Youtubes";
-import Syllabus from "./components/layout/Syllabus";
-import Contact from "./components/layout/Contact";
+import Home from './components/Home';
+import Students from './components/students/Students';
+import Resources from './components/resources/Resources';
+import Profile from './components/profile/Profile';
+import Assignments from './components/assignments/Assignments';
+import Youtubes from './components/youtube/Youtubes';
+import Syllabus from './components/layout/Syllabus';
+import Contact from './components/layout/Contact';
+import StudentQuiz from './components/quiz/StudentQuiz';
 
 class App extends Component {
   render() {
@@ -21,15 +22,15 @@ class App extends Component {
         <div>
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/students" component={Students} />
-            <Route exact path="/resources" component={Resources} />
-            <Route exact path="/user/:uid" component={Profile} />
-            <Route exact path="/assignments" component={Assignments} />
-            <Route axact path="/videos" component={Youtubes} />
-            <Route exact path="/syllabus" component={Syllabus} />
-            <Route exact path="/contact" component={Contact} />
-            {/* <Route exact path="grade" component={} */}
+            <Route exact path='/' component={Home} />
+            <Route exact path='/students' component={Students} />
+            <Route exact path='/resources' component={Resources} />
+            <Route exact path='/user/:uid' component={Profile} />
+            <Route exact path='/assignments' component={Assignments} />
+            <Route axact path='/videos' component={Youtubes} />
+            <Route exact path='/syllabus' component={Syllabus} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/quiz' component={StudentQuiz} />
           </Switch>
         </div>
       </Router>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,144 +1,137 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import $ from "jquery";
-import Login from "../auth/Login";
-import Register from "../auth/Register";
-import Logout from "../auth/Logout";
-import { connect } from "react-redux";
-import { loggedIn } from "../../actions/userActions";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import $ from 'jquery';
+import Login from '../auth/Login';
+import Register from '../auth/Register';
+import Logout from '../auth/Logout';
+import { connect } from 'react-redux';
+import { loggedIn } from '../../actions/userActions';
 
-class Navbar extends Component {
-  collapse = () => {
-    $(".collapse").collapse("hide");
+const Navbar = props => {
+  const collapse = () => {
+    $('.collapse').collapse('hide');
   };
 
-  componentDidMount() {
-    this.props.loggedIn().then(() => {
-      // console.log(this.props.currentUser);
-    });
-  }
+  const { currentUser } = props;
 
-  render() {
-    const { currentUser } = this.props;
-
-    return (
-      <div className="front" id="top">
-        <nav className="navbar navbar-expand-lg navbar-dark">
-          <button
-            className="navbar-toggler"
-            data-toggle="collapse"
-            data-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarTogglerDemo02"
-            onClick={this.collapse}
-          >
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/students">
-                  Students
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://docs.google.com/spreadsheets/d/1B34dAaxyfY_Z6an_bQ0nB8HnSewuqByvBxcy5f7-b0A/edit?usp=sharing"
-                >
-                  Curriculum
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/assignments">
-                  Assignments
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/resources">
-                  Resources
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/videos">
-                  Videos
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="https://msimbo.slack.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Slack
-                </a>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">
-                  Contact us
-                </Link>
-              </li>
-            </ul>
-            {currentUser === 0 &&
-              <button
-                className="btn menu-button"
-                data-toggle="modal"
-                data-target="#loginModal"
-                onClick={this.login}
+  return (
+    <div className='front' id='top'>
+      <nav className='navbar navbar-expand-lg navbar-dark'>
+        <button
+          className='navbar-toggler'
+          data-toggle='collapse'
+          data-target='#navbarTogglerDemo02'
+          aria-controls='navbarTogglerDemo02'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div
+          className='collapse navbar-collapse'
+          id='navbarTogglerDemo02'
+          onClick={collapse}
+        >
+          <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/'>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/students'>
+                Students
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://docs.google.com/spreadsheets/d/1B34dAaxyfY_Z6an_bQ0nB8HnSewuqByvBxcy5f7-b0A/edit?usp=sharing'
               >
-                Login
-              </button>}
-            {currentUser !== 0 &&
-              <React.Fragment>
-                <Link
-                  to={`/user/${currentUser._id}`}
-                  className="btn menu-button"
-                >
-                  <i className="fas fa-user" />
-                </Link>
-                <button
-                  className="btn menu-button"
-                  data-toggle="modal"
-                  data-target="#logoutModal"
-                >
-                  Logout
-                </button>
-              </React.Fragment>}
-            {currentUser !== 0 &&
-              currentUser.admin &&
-              <button
-                className="btn menu-button"
-                data-toggle="modal"
-                data-target="#registerModal"
+                Curriculum
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <Link className='nav-link' to='/assignments'>
+                Assignments
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link className='nav-link' to='/resources'>
+                Resources
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/videos'>
+                Videos
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='nav-link'
+                href='https://msimbo.slack.com'
+                target='_blank'
+                rel='noopener noreferrer'
               >
-                Register
-              </button>}
-          </div>
-        </nav>
-        <Login />
-        <Register />
-        <Logout />
-      </div>
-    );
-  }
-}
+                Slack
+              </a>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/contact'>
+                Contact us
+              </Link>
+            </li>
+          </ul>
+          {currentUser === 0 && (
+            <button
+              className='btn menu-button'
+              data-toggle='modal'
+              data-target='#loginModal'
+            >
+              Login
+            </button>
+          )}
+          {currentUser !== 0 && (
+            <React.Fragment>
+              <Link to={`/user/${currentUser._id}`} className='btn menu-button'>
+                <i className='fas fa-user' />
+              </Link>
+              <button
+                className='btn menu-button'
+                data-toggle='modal'
+                data-target='#logoutModal'
+              >
+                Logout
+              </button>
+            </React.Fragment>
+          )}
+          {currentUser !== 0 && currentUser.admin && (
+            <button
+              className='btn menu-button'
+              data-toggle='modal'
+              data-target='#registerModal'
+            >
+              Register
+            </button>
+          )}
+        </div>
+      </nav>
+      <Login />
+      <Register />
+      <Logout />
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser
 });
 
-export default connect(mapStateToProps, { loggedIn })(Navbar);
+export default connect(
+  mapStateToProps,
+  { loggedIn }
+)(Navbar);
