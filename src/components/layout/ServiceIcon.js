d4fb8f58cf25a48ba1e08b0b7703ed63310cb20f
Shiyu Wang <shiyu3169@gmail.com>
2019-07-01 10:46:45 -0400

    rewrite services into react hooks and change props and remove redundent functions

--- src/components/layout/ServiceIcon.js
+++ src/components/layout/ServiceIcon.js
@@ -1,49 +1,53 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-class ServiceIcon extends Component {
-    state = {
-        show: false
-    };
-
-    mouseOver = () => {
-        this.setState({
-            show: true
-        });
-    };
-
-    mouseOut = () => {
-        this.setState({
-            show: false
-        });
-    };
-
-    render() {
-        const { iconClass, title, description, onClick } = this.props;
-        const { show } = this.state;
-        return (
-            <div>
-                <span
-                    className="pointer"
-                    onClick={onClick}
-                    onMouseOver={this.mouseOver}
-                    onMouseOut={this.mouseOut}
-                >
-                    <i className={iconClass} />
-                    <h3>{title}</h3>
-                    <p className="transition" style={{ opacity: show ? 1 : 0 }}>
-                        {description}
-                    </p>
-                </span>
-            </div>
-        );
-    }
-}
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+const ServiceIcon = ({ iconClass, title, description, onClick, to, href }) => {
+  const [show, setShow] = useState(false);
+  return to ? (
+    <Link to={to} className='text-light'>
+      <span
+        className='pointer'
+        onClick={onClick}
+        onMouseOver={() => setShow(true)}
+        onMouseOut={() => setShow(false)}
+      >
+        <i className={iconClass} />
+        <h3>{title}</h3>
+        <p className='transition' style={{ opacity: show ? 1 : 0 }}>
+          {description}
+        </p>
+      </span>
+    </Link>
+  ) : (
+    <a
+      href={href}
+      className='text-light'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      <span
+        className='pointer'
+        onClick={onClick}
+        onMouseOver={() => setShow(true)}
+        onMouseOut={() => setShow(false)}
+      >
+        <i className={iconClass} />
+        <h3>{title}</h3>
+        <p className='transition' style={{ opacity: show ? 1 : 0 }}>
+          {description}
+        </p>
+      </span>
+    </a>
+  );
+};
 
 ServiceIcon.prototypes = {
-    iconClass: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+  iconClass: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  to: PropTypes.string,
+  href: PropTypes.string
 };
 
 export default ServiceIcon;
--- src/components/layout/Services.js
+++ src/components/layout/Services.js
@@ -1,82 +1,55 @@
-import React, { Component } from "react";
-import ServiceIcon from "./ServiceIcon";
-import { Redirect } from "react-router-dom";
+import React from 'react';
+import ServiceIcon from './ServiceIcon';
 
-class Services extends Component {
-  state = {
-    redirect: false,
-    path: ""
-  };
-
-  navigate = url => {
-    var win = window.open(url, "_blank");
-    win.focus();
-  };
-
-  redirect = path => {
-    this.setState({
-      redirect: true,
-      path: path
-    });
-  };
-
-  render() {
-    if (this.state.redirect) {
-      return <Redirect push to={this.state.path} />;
-    }
-
-    return (
-      <React.Fragment>
-        <section className="services">
-          <div className="container grid-3 center">
-            <ServiceIcon
-              iconClass="fas fa-users fa-3x"
-              title="Students"
-              description="Students list"
-              onClick={this.redirect.bind(this, "/students")}
-            />
-            <ServiceIcon
-              iconClass="fas fa-graduation-cap fa-3x"
-              title="Curriculum"
-              description="Course Schedule"
-              onClick={this.navigate.bind(
-                this,
-                "https://docs.google.com/spreadsheets/d/1B34dAaxyfY_Z6an_bQ0nB8HnSewuqByvBxcy5f7-b0A/edit?usp=sharing"
-              )}
-            />
-            <ServiceIcon
-              iconClass="fab fa-youtube fa-3x"
-              title="Videos"
-              description="Class Videos on Youtube"
-              onClick={this.redirect.bind(this, "/videos")}
-            />
-          </div>
-        </section>
-        <section className="services">
-          <div className="container grid-3 center">
-            <ServiceIcon
-              iconClass="fas fa-pencil-ruler fa-3x"
-              title="Assignments"
-              description="Student Assignments"
-              onClick={this.redirect.bind(this, "/assignments")}
-            />
-            <ServiceIcon
-              iconClass="fas fa-archive fa-3x"
-              title="Resources"
-              description="Course slides and materials"
-              onClick={this.redirect.bind(this, "/resources")}
-            />
-            <ServiceIcon
-              iconClass="fab fa-slack fa-3x"
-              title="Slack"
-              description="Contact instructor or students"
-              onClick={this.navigate.bind(this, "https://msimbo.slack.com")}
-            />
-          </div>
-        </section>
-      </React.Fragment>
-    );
-  }
+function Services() {
+  return (
+    <>
+      <section className='services'>
+        <div className='container grid-3 center'>
+          <ServiceIcon
+            iconClass='fas fa-users fa-3x'
+            title='Students'
+            description='Students list'
+            to='/students'
+          />
+          <ServiceIcon
+            iconClass='fas fa-graduation-cap fa-3x'
+            title='Curriculum'
+            description='Course Schedule'
+            href='https://docs.google.com/spreadsheets/d/1B34dAaxyfY_Z6an_bQ0nB8HnSewuqByvBxcy5f7-b0A/edit?usp=sharing'
+          />
+          <ServiceIcon
+            iconClass='fab fa-youtube fa-3x'
+            title='Videos'
+            description='Class Videos on Youtube'
+            to='/videos'
+          />
+        </div>
+      </section>
+      <section className='services'>
+        <div className='container grid-3 center'>
+          <ServiceIcon
+            iconClass='fas fa-pencil-ruler fa-3x'
+            title='Assignments'
+            description='Student Assignments'
+            to='/assignments'
+          />
+          <ServiceIcon
+            iconClass='fas fa-archive fa-3x'
+            title='Resources'
+            description='Course slides and materials'
+            to='/resources'
+          />
+          <ServiceIcon
+            iconClass='fab fa-slack fa-3x'
+            title='Slack'
+            description='Contact instructor or students'
+            href='https://msimbo.slack.com'
+          />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Services;
