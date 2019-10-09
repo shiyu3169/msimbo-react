f2df8906f0d79aa2ac0ffe54ac65a3400f232a55
Shiyu Wang <shiyu3169@gmail.com>
2019-10-09 16:20:15 -0400

    fix server

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/videos", require("./routes/videos"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
